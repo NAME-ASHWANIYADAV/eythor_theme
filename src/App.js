4471e2a9dba2d5618d2cf68e2c3cff60e9f5587c
ashwani yadav <22ashwaniyadav@gmail.com>
2024-12-01 23:30:13 +0530

    adding code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import About from './pages/About';
-import Offerings from './pages/Offerings';
+// import Offerings from './pages/Offerings';
 import Header from './components/Header'; // Import the Header component
-import SolutionPage from './pages/SolutionPage';
-import MainPage from './pages/MainPage';
-import ContactUs from './pages/ContactUs';
+// import SolutionPage from './pages/SolutionPage';
+// import MainPage from './pages/MainPage';
+// import ContactUs from './pages/ContactUs';
 import Footer from './components/Footer';
 
 function App() {
@@ -18,10 +18,10 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<About />} />
-          <Route path="/offerings" element={<Offerings />} />
+          {/* <Route path="/offerings" element={<Offerings />} />
           <Route path="/solutions" element={<SolutionPage/>} />
           <Route path="/Team" element={<MainPage/>} />
-          <Route path="/contact" element={<ContactUs/>} />
+          <Route path="/contact" element={<ContactUs/>} /> */}
         </Routes>
         <Footer />
       </div>
